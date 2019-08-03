be511f164d7e11e879e52fc32e7530d419acf32a
Jim Chung <jchung225@gmail.com>
2019-08-03 10:25:05 -0400

    still having trouble printing info on the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,46 @@
 
     var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-var wins = 0;
-var loses = 0;
-var guessesLeft= 10;
-var userGuess = "";
-var computerGuess = "";
-var guessSoFar =[];
+    var winsText = document.getElementById("wins");
+    var lossesText = document.getElementById("losses");
+    var guessesLeft= document.getElementById("GuessesLeft");
+    var guessSoFar = document.getElementById("GuessSoFar");
+    var userGuess = "";
+    var computerGuess = "";
+    var winscount = 0;
+    var lossescount = 0;
+    var alreadyGuessed = [];
 
-document.onkeyup = function(event) {
-    guessesLeft--;
-   
-    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(computerGuess);
+
     
-    userGuess = event.key.toLowerCase();
+// starts function on key event.
+    document.onkeyup = function(event) {
+        guessesLeft--;
+       
+        var userGuess = event.key;
+        userGuess.toLowerCase();
+         console.log(guessSoFar);
 
-	console.log(userGuess);
-
-	if (userGuess === computerGuess){
-        wins++;
-		restart();
-    } 
-	else if (guessesLeft === 0) {
-        loses++;
-		loses.textcontent = loses;
-		restart();
-	}
-  };
-  
+        // random guess from computer.
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log(computerGuess);
+        
+    
+        console.log(userGuess);
+    
+        if (userGuess === computerGuess){
+            console.log("you win");
+            alert("You're a Winner");
+            winsText++;
+        } 
+        else {
+            console.log("wrong Answer");
+        }
+        
+        
+        if (guessesLeft === 0) {
+            console.log("You lose");
+            alert("You're a Loser");
+            lossesText++;
+        }
+      };
